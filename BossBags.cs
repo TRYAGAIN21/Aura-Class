32b8fa5e7e7953618fa10f845f93cd83d84b0042
TRYAGAIN21 <84547832+TRYAGAIN21@users.noreply.github.com>
2021-06-24 08:57:26 -0700

    Code Cleanup

--- BossBags.cs
+++ BossBags.cs
@@ -9,12 +9,10 @@ namespace AuraClass
 	{
 		public override void OpenVanillaBag(string context, Player player, int arg)
 		{
-			// This method shows adding items to Fishrons boss bag. 
-			// Typically you'll also want to also add an item to the non-expert boss drops, that code can be found in ExampleGlobalNPC.NPCLoot. Use this and that to add drops to bosses.
 			if (context == "bossBag" && arg == ItemID.QueenBeeBossBag)
 			{
 				if (Main.rand.Next(4) == 0)
-                {
+				{
 					player.QuickSpawnItem(mod.ItemType("HiveAura"));
 				}
 			}
